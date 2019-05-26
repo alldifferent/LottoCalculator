7f252ece2ab5c6e35265a4e8611d6a275d41952f
alldifferent <alldifferent@naver.com>
2019-05-26 23:56:24 +0900

    Initial commit

---
+++ app/src/main/java/com/alldi/lottocalculator/MainActivity.java
@@ -0,0 +1,34 @@
+package com.alldi.lottocalculator;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.alldi.lottocalculator.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding act;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void setupEvents() {
+        
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+        act = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+    }
+}
